23022ca18eefaa9198851f893936c1f0862f0a01
Hamzeh Qutishat <hamzehqutishat@outlook.com>
2025-02-18 09:57:29 +0100

    39

--- dist/js/main.js
+++ dist/js/main.js
@@ -148,6 +148,41 @@
     $('.venobox').venobox();
   });
 
+  // Initialize AOS (Animate On Scroll) library
+  AOS.init({
+    duration: 800,
+    easing: 'slide',
+    once: true
+  });
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        e.preventDefault();
+
+        var scrollto = target.offset().top;
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu, .mobile-nav').length) {
+          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-overly').fadeOut();
+        }
+        return false;
+      }
+    }
+  });
+
 function toggleChatbox() {
     const chatbox = document.getElementById('chatbox');
     if (chatbox.style.display === 'none' || chatbox.style.display === '') {
